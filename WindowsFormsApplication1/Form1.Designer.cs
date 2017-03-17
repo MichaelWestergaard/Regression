417ed7a34f2c886c0569d90d6d8e647584f85b20
Michael <Micha@LAPTOP-0N8IQMUR>
2017-03-17 10:12:59 +0100

    gg

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.y4 = new System.Windows.Forms.TextBox();
+            this.x4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(221, 2);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(668, 447);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart2";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(9, 40);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 20);
+            this.x1.TabIndex = 0;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(115, 40);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(100, 20);
+            this.y1.TabIndex = 1;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(9, 66);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(100, 20);
+            this.x2.TabIndex = 2;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(115, 66);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(100, 20);
+            this.y2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Beregn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(115, 92);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(100, 20);
+            this.y3.TabIndex = 5;
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(9, 92);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(100, 20);
+            this.x3.TabIndex = 4;
+            // 
+            // y4
+            // 
+            this.y4.ForeColor = System.Drawing.Color.Black;
+            this.y4.Location = new System.Drawing.Point(115, 118);
+            this.y4.Name = "y4";
+            this.y4.Size = new System.Drawing.Size(100, 20);
+            this.y4.TabIndex = 7;
+            // 
+            // x4
+            // 
+            this.x4.Location = new System.Drawing.Point(9, 118);
+            this.x4.Name = "x4";
+            this.x4.Size = new System.Drawing.Size(100, 20);
+            this.x4.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 144);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Linær";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(115, 144);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Eksponentiel";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(9, 168);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(78, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Logaritmisk";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(115, 168);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Potens";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.y4);
+            this.Controls.Add(this.x4);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.TextBox y4;
+        private System.Windows.Forms.TextBox x4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,14 +7,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        double sumX = 0.0, sumY = 0.0, sumX2 = 0.0;
+        List<double> X = new List<double>();
+        List<double> Y = new List<double>();
+
+        public double intercept, slope;
+        public string tendensline;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            X.Clear();
+            Y.Clear();
+
+            if (x1.Text != "" || y1.Text != "")
+            {
+                X.Add(double.Parse(x1.Text));
+                Y.Add(double.Parse(y1.Text));
+            }
+            if (x2.Text != "" || y2.Text != "")
+            {
+                X.Add(double.Parse(x2.Text));
+                Y.Add(double.Parse(y2.Text));
+            }
+            if (x3.Text != "" || y3.Text != "")
+            {
+                X.Add(double.Parse(x3.Text));
+                Y.Add(double.Parse(y3.Text));
+            }
+            if (x4.Text != "" || y4.Text != "")
+            {
+                X.Add(double.Parse(x4.Text));
+                Y.Add(double.Parse(y4.Text));
+            }
+
+            if (radioButton1.Checked)
+            {
+                tendensline = "Linear";
+            }
+            else if (radioButton2.Checked)
+            {
+                tendensline = "Exponential";
+            }
+            else if (radioButton3.Checked)
+            {
+                tendensline = "Logarithmic";
+            }
+            else if (radioButton4.Checked)
+            {
+                tendensline = "Power";
+            }
+
+            if(tendensline == "Linear")
+            {
+                Linear();
+            }
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Series");
+            chart1.Series.Add("Tendenslinje");
+            chart1.Series["Tendenslinje"].ChartType = SeriesChartType.Line;
+            chart1.Series["Series"].ChartType = SeriesChartType.Point;
+            
+            for (int i = 0; i < X.Count; i++)
+            {
+                chart1.Series["Series"].Points.AddXY(X[i], Y[i]);
+            }
+
+            chart1.DataManipulator.FinancialFormula(FinancialFormula.Forecasting, tendensline + ", 1, false, false", chart1.Series[0], chart1.Series["Tendenslinje"]);
+        }
+        
+        public void Linear()
+        {
+            for(int i = 0; i < X.Count; i++)
+            {
+                sumX += X[i];
+                sumY += Y[i];
+                sumX2 += X[i] * X[i];
+            }
+
+            double x = sumX / X.Count;
+            double y = sumY / X.Count;
+
+            double xx = 0.0, yy = 0.0, xy = 0.0;
+
+            for(int i = 0; i < X.Count; i++)
+            {
+                xx += (X[i] - x) * (X[i] - x);
+                yy += (Y[i] - y) * (Y[i] - y);
+                xy += (X[i] - x) * (Y[i] - y);
+            }
+                        
+            slope = xy / xx;
+            intercept = y - slope * x;
+
+            double rss = 0.0;
+            double ssr = 0.0;
+            for (int i = 0; i < X.Count; i++)
+            {
+                double fit = slope * X[i] + intercept;
+                rss += (fit - X[i]) * (fit - X[i]);
+                ssr += (fit - y) * (fit - y);
+            }
+            
+            float r2 = (float) (ssr / yy);
+            label3.Text = "y = " + (float) intercept + "+" + (float) slope + "x; R^2=" + r2;
+            
+        }
+
     }
 }
